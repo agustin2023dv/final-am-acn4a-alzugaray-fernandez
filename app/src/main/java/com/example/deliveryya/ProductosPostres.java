1bee27941df30230c37b22548cd23694053e9db3
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2023-11-25 10:28:41 +0100

    add: avances con productos_postres falta desarrollar logica

--- app/src/main/java/com/example/deliveryya/ProductosPostres.java
+++ app/src/main/java/com/example/deliveryya/ProductosPostres.java
@@ -1,6 +1,9 @@
 package com.example.deliveryya;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -20,13 +23,7 @@ public class ProductosPostres extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_productos_postres);
 
-
-        listaProductos = new ArrayList<>();
-
-
-        listaProductos.add(new Producto("Producto 1", "Descripción 1", 1, 10.99));
-        listaProductos.add(new Producto("Producto 2", "Descripción 2", 2, 19.99));
-        listaProductos.add(new Producto("Producto 3", "Descripción 3", 3, 5.99));
+        View layoutProductoView = findViewById(R.id.layoutProducto);
 
 
     }
