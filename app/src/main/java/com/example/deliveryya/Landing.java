7572897f34290840a16f2bdc5e36a42d30347e8e
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2023-11-25 09:05:59 +0100

    add: accion para que vaya de Landing a Login al apretar botor COntinuar

--- app/src/main/java/com/example/deliveryya/Landing.java
+++ app/src/main/java/com/example/deliveryya/Landing.java
@@ -2,7 +2,10 @@ package com.example.deliveryya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Landing extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class Landing extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        Button btnContinuar = findViewById(R.id.btnContinuar);
+
+
+        btnContinuar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
