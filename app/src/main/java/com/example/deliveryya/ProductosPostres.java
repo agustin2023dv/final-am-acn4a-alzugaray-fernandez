001061a4e8a6ffcf938f5968675b4cb7b7681891
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2023-11-25 11:13:49 +0100

    add: cuando se hace click en el carrito de compras se redirecciona a la activity carrito_compras

--- app/src/main/java/com/example/deliveryya/ProductosPostres.java
+++ app/src/main/java/com/example/deliveryya/ProductosPostres.java
@@ -1,8 +1,10 @@
 package com.example.deliveryya;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,22 +18,55 @@ import java.util.List;
 
 public class ProductosPostres extends AppCompatActivity {
 
-
+    List<Producto> carritoCompras = new ArrayList<Producto>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_productos_postres);
 
 
+        ImageView imgCart = findViewById(R.id.img_cart);
+
+        // Configurar un clic en el ImageView
+        imgCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Crear un Intent para abrir la actividad "activity_carrito_compras.xml"
+                Intent intent = new Intent(ProductosPostres.this, CarritoCompras.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
         Button btnComprarHeladoVainilla = findViewById(R.id.btnComprarHeladoVainilla);
         btnComprarHeladoVainilla.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Maneja el evento de clic del botón aquí
-                Toast.makeText(ProductosPostres.this, "Has comprado Helado de Vainilla", Toast.LENGTH_SHORT).show();
+
+                Toast.makeText(ProductosPostres.this, "Has agregado Helado de Vainilla al carrito de compra", Toast.LENGTH_SHORT).show();
+
             }
         });
 
+
+
+
+
+
+
+
+        Button btnComprarHeladoChocolate = findViewById(R.id.btnComprarHeladoChocolate);
+        btnComprarHeladoChocolate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(ProductosPostres.this, "Has agregado Helado de Chocolate al carrito de compra", Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
     }
 }
