5236c8ccb655a9db5967539e714ae2c4adedb573
lualzugaray <lucia.alzugaray24@gmail.com>
2023-11-27 01:48:55 -0300

    add: se agrega menu de opciones e imagenes en botones

---
+++ app/src/main/java/com/example/deliveryya/MenuOpciones.java
@@ -0,0 +1,81 @@
+package com.example.deliveryya;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.content.DialogInterface;
+import android.content.Intent;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+
+import android.widget.SearchView;
+import android.widget.TableLayout;
+
+import androidx.appcompat.app.AlertDialog;
+
+public class MenuOpciones extends AppCompatActivity{
+
+    private ImageView carroCompras;
+    private EditText textoBuscador;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_opciones);
+
+
+        carroCompras = findViewById(R.id.img_cart);
+        carroCompras.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Carro de compras vacio", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        textoBuscador = findViewById(R.id.etBuscador);
+
+        textoBuscador.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    textoBuscador.setHint("");
+                } else {
+                    textoBuscador.setHint("¿Qué estás buscando?");
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode==event.KEYCODE_BACK){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("¿Desea salir?").setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent(Intent.ACTION_MAIN);
+                    intent.addCategory(Intent.CATEGORY_HOME);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+            }).setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            builder.show();
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
